1f289d7e750713e5b8bd5f1b7955fadc90bc3753
cse-mominul <cse.mominul.islam@gmail.com>
2026-04-12 22:08:23 +0600

    design updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -354,7 +354,7 @@ const Navbar = () => {
                 </svg>
                 <span>Wishlist</span>
                 {wishlistCount > 0 && (
-                  <span className="absolute -top-2 -right-3 bg-blue-500 text-white text-[10px] w-5 h-5 rounded-full flex items-center justify-center font-bold">
+                  <span className="absolute -top-2 -right-3 bg-red-500 text-white text-[10px] w-5 h-5 rounded-full flex items-center justify-center font-bold">
                     {wishlistCount}
                   </span>
                 )}
